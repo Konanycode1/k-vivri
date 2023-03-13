c32aa52c00a26e9d4381bbcf1569368cd2f669c3
kouame konan abraham <abrahamkonany@gmail.com>
2023-03-13 17:05:54 +0000

    Update authentik.js

--- js/authentik.js
+++ js/authentik.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    let data = localStorage.getItem("SessionUser")
-    console.log()
+    let data = localStorage.getItem("SessionUser");
+    let verifSuper = localStorage.getItem("superAdmin");
+    let verifAdmin =  localStorage.getItem("admin");
+    verifAdmin = JSON.parse(verifAdmin);
+    verifSuper = JSON.parse(verifSuper);
+    data = JSON.parse(data);
+    // data = data.find(element => element.admin);
+
+    // if( data.user == verifSuper.admin || data.user == verifSuper.admin )
+
+
+
+    
+
+
+    
 })
