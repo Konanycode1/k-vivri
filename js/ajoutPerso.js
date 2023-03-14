7d326391da67aaae90823a109804f260cc5d79d5
kouame konan abraham <abrahamkonany@gmail.com>
2023-03-14 10:11:31 +0100

    ajout

--- js/ajoutPerso.js
+++ js/ajoutPerso.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", ()=>{
+
+    // ajout personnel
     let form = document.getElementById('formAjoutPerso');
     let btnAjout = document.getElementById('btnAjout');
     let nom = document.getElementById('nom')
@@ -7,16 +9,14 @@ document.addEventListener("DOMContentLoaded", ()=>{
     let domaine = document.getElementById('domaine');
     let attache = document.getElementById('tach');
     let tel = document.getElementById('numero');
-    
 
-
-
+    // soumettre et stopper l'action du form
     form.addEventListener('submit', (e)=>{
          e.preventDefault();
     })
+    // ajouter les information du personnel
     btnAjout.addEventListener('click', ()=>{
         let perso = localStorage.getItem("perso");
-        let recup = [];
         let data = {
             nom : nom.value,
             preneom : preneom.value,
@@ -25,7 +25,6 @@ document.addEventListener("DOMContentLoaded", ()=>{
             attache : attache.value,
             tel : tel.value
         }
-
         if(perso !== null){
             perso = JSON.parse(perso);
             perso.push(data);
@@ -37,9 +36,45 @@ document.addEventListener("DOMContentLoaded", ()=>{
             perso.push(data);
             console.log(perso)
             localStorage.setItem("perso",JSON.stringify(perso))
-        }
-       
+        } 
     })
-    
+
+let formRegistre = document.getElementById("K-form-registre");
+let btnEnregist = document.querySelector(".btn-regist");
+let tache = document.getElementById("tache");
+
+formRegistre.addEventListener("submit", (e)=>{
+    e.preventDefault();
+})
+btnEnregist.addEventListener("click", ()=>{
+    const event = new Date();
+const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+    let ajoutTache = {
+        tache : tache.value,
+        dateAjout: event.toLocaleDateString(),
+        dateTermine: "En cours"
+    }
+    let dataRegist = localStorage.getItem("tache");
+    dataRegist = JSON.parse(dataRegist);
+    console.log(dataRegist);
+    if(dataRegist !== null){
+        let isfount = dataRegist.some(element => element.tache === tache.value)
+        if(isfount){
+            alert("Tâche non terminer");
+        }
+        else{
+            dataRegist.push(ajoutTache)
+            localStorage.setItem("tache",JSON.stringify(dataRegist));
+            window.location.reload()
+        }
+    }
+    else{
+        dataRegist = [];
+        dataRegist.push(ajoutTache);
+        localStorage.setItem("tache", JSON.stringify(dataRegist));
+        window.location.reload();
+    }
+
+})
 
 })
